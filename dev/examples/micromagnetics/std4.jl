6f6a5cbfee676b31db3797d942510ca62f5221a6
Documenter.jl <documenter@juliadocs.github.io>
2022-10-04 09:18:23 +0800

    build based on 5c0acec

---
+++ dev/examples/micromagnetics/std4.jl
@@ -0,0 +1,82 @@
+using JuMag
+using Printf
+using NPZ
+
+JuMag.cuda_using_double(true)
+
+mesh =  FDMeshGPU(nx=200, ny=50, nz=1, dx=2.5e-9, dy=2.5e-9, dz=3e-9)
+
+function relax_system(mesh)
+  #Create a simulation with 'SD' driver
+  sim = Sim(mesh, name="std4_relax", driver="SD")
+
+  set_Ms(sim, 8.0e5)
+  sim.driver.min_tau = 1e-10
+
+  add_exch(sim, 1.3e-11)
+  add_demag(sim)
+
+  init_m0(sim, (1, 0.25, 0.1))
+
+  relax(sim, maxsteps=5000, stopping_dmdt=0.01)
+  npzwrite("std4_m0.npy", Array(sim.spin))
+end
+
+function apply_field1(mesh)
+  sim = Sim(mesh, name="std4_dyn")
+  set_Ms(sim, 8.0e5)
+  sim.driver.alpha = 0.02
+  sim.driver.gamma = 2.211e5
+
+  mT = 0.001 / (4*pi*1e-7)
+  add_exch(sim, 1.3e-11)
+  add_demag(sim)
+  add_zeeman(sim, (-24.6*mT, 4.3*mT, 0))
+
+  init_m0(sim, npzread("std4_m0.npy"))
+
+  for i=1:100
+    t = 1e-11*i
+    run_until(sim, t)
+    @info @sprintf("Run step2 up to time %g", t)
+  end
+end
+
+println("Start to run step1 !")
+relax_system(mesh)
+println("Start to run step2 !")
+apply_field1(mesh)
+println("Done!")
+
+using DelimitedFiles
+using CairoMakie
+
+function plot_m()
+    folder = @__DIR__
+    data = readdlm(folder*"/assets/std4_dyn.txt", skipstart=2)
+    oommf = readdlm(folder*"/assets/std4_oommf.txt")
+
+    fig = Figure(resolution = (800, 480))
+    ax = Axis(fig[1, 1],
+        xlabel = "Time (ns)",
+        ylabel = "m"
+    )
+
+    lines!(ax, oommf[:,1]*1e9, oommf[:,2], label="OOMMF")
+    lines!(ax, oommf[:,1]*1e9, oommf[:,3])
+    lines!(ax, oommf[:,1]*1e9, oommf[:,4])
+
+    scatter!(ax, data[:,2]*1e9, data[:,4], markersize = 6, label="JuMag")
+    scatter!(ax, data[:,2]*1e9, data[:,5], markersize = 6)
+    scatter!(ax, data[:,2]*1e9, data[:,6], markersize = 6)
+
+    axislegend()
+
+    return fig
+
+end
+
+plot_m()
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
---
+++ dev/examples/micromagnetics/stoner_wohlfarth.jl
@@ -0,0 +1,68 @@
+using JuMag
+
+function relax_sw_loop()
+    #We create a mesh for a cubic geometry 4nm x 4nm x 4nm
+    mesh = FDMeshGPU(nx=4, ny=4, nz=4, dx=1e-9, dy=1e-9, dz=1e-9)
+
+    #We create a simulation with the SD driver.
+    sim = Sim(mesh, name="sw_cubic", driver="SD")
+
+    #We set the saturation magnetization of the system.
+    set_Ms(sim, 1.0e6)
+
+    #Set the exchange constant
+    add_exch(sim, 1.3e-11)
+
+    #Set the initial state of the system.
+    init_m0(sim, (-1, 1, 0))
+
+    #Add the anisotropy
+    add_anis(sim, 5e4, axis=(1,1,0))
+
+    #Set the zeeman field.
+    add_zeeman(sim,(0,0,0))
+
+    #For each field, we relax the system to obtain its equilibrium state.
+    for i=-100:5:100
+        Hx = i*mT # A/m
+
+        update_zeeman(sim, (Hx,0,0))
+
+        #Relax the system with stopping_dmdt=0.05, the write_data function will be called if save_m_every is positive
+        relax(sim, maxsteps=10000, stopping_dmdt=0.05, save_m_every=10000)
+
+    end
+end
+
+relax_sw_loop()
+
+using DelimitedFiles
+using CairoMakie
+
+function plot_loop()
+    folder = @__DIR__
+    data = readdlm(folder*"/sw_cubic.txt", skipstart=2)
+    m, H = data[:, 3], data[:, 8]
+
+    fig = Figure(resolution = (800, 500))
+    ax = Axis(fig[1, 1],
+        xlabel = "H (A/m)",
+        ylabel = "mx"
+    )
+
+    lines!(ax, H, m, color = :blue)
+    scatter!(ax, H, m, markersize = 6, color = :blue)
+    lines!(ax, -H, -m, color = :blue)
+    scatter!(ax, -H, -m, markersize = 6, color = :blue)
+
+    expected = 39788.736 # A/m
+    vlines!(ax, [expected, -expected], color = :red, linestyle = :dash)
+
+    return fig
+
+end
+
+plot_loop()
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
